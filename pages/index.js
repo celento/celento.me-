5d8617a94fea47ec8e8097678e8a2f35a68f9954
Celento C George <celentocgeo@gmail.com>
2022-08-07 20:33:18 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,11 +14,9 @@ export default function Home() {
       <main className="flex flex-col items-center justify-center w-full flex-1 px-10 max-w-3xl">
         
         <p className="mt-3 text-2xl">
-         <span className="text-yellow-500">Hola,</span>
-         <p className="my-1">After years of procrastination, I finally decided to update this website. Hopefully, sometime soon.</p>
-         <p className="text-base my-2">Meanwhile, checkout my <a target="_blank" className=" text-yellow-500 hover:underline transition-all hover:opacity-80" href="https://www.twitter.com/celento">Twitter</a></p>
-
-         <p className="italic text-base">You stay classy</p>
+ 
+         <p className="my-1">Coming Soon...</p>
+       
         </p>
 
        
